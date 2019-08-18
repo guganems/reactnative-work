076e38f3b3ed9a556640b763cc9b32fea9f65a08
gnems <g_nemsveridze@cu.edu.ge>
2019-08-19 00:38:36 +0400

    Props

--- helloworld/App.js
+++ helloworld/App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,9 +18,13 @@ const instructions = Platform.select({
 
 export default class App extends Component {
   render() {
+    const imageInfo = {
+      uri: "https://scontent.ftbs3-1.fna.fbcdn.net/v/t1.0-9/68892295_2542809562429443_1040231737537331200_n.png?_nc_cat=1&_nc_oc=AQnc9XO-nyleDoAr867slt0cHkvUv1ocVas7bwaK1_KDMJg9_aX0hD6RWcFmtUKur_M&_nc_ht=scontent.ftbs3-1.fna&oh=080d80796fa43af7597a04b9f4344f67&oe=5DCCD693"
+    }
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Hello, World!</Text>
+        <Image source={imageInfo} />
       </View>
     );
   }
